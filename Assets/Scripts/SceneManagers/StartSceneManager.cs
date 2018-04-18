ff8d432f248444ce1226635b6f11aaf9fb978942
na1814 <na1814@ic.ac.uk>
2018-04-18 18:55:44 +0100

    Ready for first protocol

---
+++ Assets/Scripts/SceneManagers/StartSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HoloToolkit.Unity;
+
+public class StartSceneManager : Singleton<StartSceneManager>
+{
+    void openTherapyRoomScene() {
+        Application.LoadLevel("TherapyRoom");
+    }
+
+}
---
+++ Assets/Scripts/Window/StartScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+
+    public GameObject userCamera;
+    public int screenMovementSpeed = 2; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetDir = userCamera.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float step = screenMovementSpeed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, -targetDir, step, 0.0f);
+
+        // Move our position a step closer to the target.
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
